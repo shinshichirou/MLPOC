499637141cdcd519a1e14dcd2be79db0dadf1a2a
Igor Tudoran <igor.tudoran@dataart.com>
2025-10-21 20:50:41 +0300

    Initial Commit

---
+++ MLPOC/MLPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLPOCApp.swift
+//  MLPOC
+//
+//  Created by Igor Tudoran on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MLPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
